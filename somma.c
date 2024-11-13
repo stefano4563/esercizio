0354675652ba1d59d7b932a6adbb9ce1da041e57
stefano <150442602+stefano4563@users.noreply.github.com>
2024-11-13 08:58:22 +0100

    Add files via upload

---
+++ somma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv)
+{
+    int numero1 = 10, numero2 = 8;
+    int somma = 0;
+
+    somma = numero1 + numero2;
+
+    printf("somma vale: %d", somma);
+
+    return 0;
+}
